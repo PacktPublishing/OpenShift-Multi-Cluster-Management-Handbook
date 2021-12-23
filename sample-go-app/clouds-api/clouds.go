a52606a4a5fafd490d82bcc9c6429ca712ac76c8
Giovanni Fontana <>
2021-12-23 10:33:58 -0300

    chapter07

--- sample-go-app/clouds-api/clouds.go
+++ sample-go-app/clouds-api/clouds.go
@@ -144,7 +144,7 @@ func returnAllClouds(w http.ResponseWriter, r *http.Request) {
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage! Version=1.0")
+	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 

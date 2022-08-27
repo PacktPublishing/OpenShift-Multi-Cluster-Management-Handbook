3f3952bd0533306a1a09ee31e3a1724beec0bf98
Giovanni Fontana <giov.fontana@gmail.com>
2022-08-27 18:01:38 -0400

    Changed to forked repo

--- chapter14/change-repo-urls.sh
+++ chapter14/change-repo-urls.sh
@@ -7,21 +7,35 @@ echo "Created by: Giovanni Fontana"
 echo ""
 echo "================================================"
 
-echo "** Input your github username: "
-read repo_user
-echo ""
-echo "** Changing ArgoCD and Pipeline manifests to your forked repo (https://github.com/$repo_user/Openshift-Multi-Cluster-management/)"
+#echo "** Input your github username: "
+#read repo_user
+#echo ""
+#echo "** Changing ArgoCD and Pipeline manifests to your forked repo (https://github.com/$repo_user/Openshift-Multi-Cluster-management/)"
+#
+#sed -i "s/PacktPublishing/$repo_user/" ./config/argocd/argocd-app-dev.yaml
+#sed -i "s/PacktPublishing/$repo_user/" ./config/argocd/argocd-app-qa.yaml
+#sed -i "s/PacktPublishing/$repo_user/" ./config/argocd/argocd-app-prod.yaml
+#sed -i "s/PacktPublishing/$repo_user/" ./config/cicd/pipelinerun/build-v1.yaml
 
-sed -i "s/PacktPublishing/$repo_user/" ./config/argocd/argocd-app-dev.yaml
-sed -i "s/PacktPublishing/$repo_user/" ./config/argocd/argocd-app-qa.yaml
-sed -i "s/PacktPublishing/$repo_user/" ./config/argocd/argocd-app-prod.yaml
-sed -i "s/PacktPublishing/$repo_user/" ./config/cicd/pipelinerun/build-v1.yaml
+echo "** Input the full path to the image in your image registry (e.g.: quay-registry-quay-openshift-operators.apps.ocpcluster.example.com/demo/quarkus-quickstarts): "
+read registry_path
+echo ""
+echo "** Changing Image Registry path:"
+
+sed -i "s/image-path-changeme/$registry_path/" ./Build/PipelineRun/quarkus-build-pr.yaml
+sed -i "s/image-path-changeme/$registry_path/" ./Deploy/PipelineRun/quarkus-build-and-deploy-pr.yaml
+sed -i "s/image-path-changeme/$registry_path/" ./DevSecOps/PipelineRun/quarkus-devsecops-v1-pr.yaml
+sed -i "s/image-path-changeme/$registry_path/" ./DevSecOps/PipelineRun/quarkus-devsecops-v2-pr.yaml
+sed -i "s/image-path-changeme/$registry_path/" ./Multicluster-Deployment/PipelineRun/quarkus-multicluster-pr.yaml
+sed -i "s/image-path-changeme/$registry_path/" ../quarkus-getting-started/k8s/overlay/v1/kustomization.yaml
+sed -i "s/image-path-changeme/$registry_path/" ../quarkus-getting-started/k8s/overlay/v1/kustomization.yaml
 
 echo ""
 echo "** Manifest files changed. Pushing changes to GitHub (please inform your GitHub user and password if asked)."
 
-git add ./config/argocd/* ./config/cicd/pipelinerun/build-v1.yaml >>/dev/null
-git commit -m 'Changed ArgoCD and Pipeline manifests to forked repo' >>/dev/null
+cd ..
+git add * >>/dev/null
+git commit -m 'Changed to forked repo' >>/dev/null
 git push -u origin main >>/dev/null
 
 echo ""

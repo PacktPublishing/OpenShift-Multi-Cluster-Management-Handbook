e5d7cb18591106f724596387970fe6cec9261d98
Giovanni Fontana <23301105+giofontana@users.noreply.github.com>
2022-08-28 00:29:39 -0400

    fix change-repo-urls.sh

--- chapter14/change-repo-urls.sh
+++ chapter14/change-repo-urls.sh
@@ -7,15 +7,25 @@ echo "Created by: Giovanni Fontana"
 echo ""
 echo "================================================"
 
-#echo "** Input your github username: "
-#read repo_user
-#echo ""
-#echo "** Changing ArgoCD and Pipeline manifests to your forked repo (https://github.com/$repo_user/Openshift-Multi-Cluster-management/)"
-#
-#sed -i "s/PacktPublishing/$repo_user/" ./config/argocd/argocd-app-dev.yaml
-#sed -i "s/PacktPublishing/$repo_user/" ./config/argocd/argocd-app-qa.yaml
-#sed -i "s/PacktPublishing/$repo_user/" ./config/argocd/argocd-app-prod.yaml
-#sed -i "s/PacktPublishing/$repo_user/" ./config/cicd/pipelinerun/build-v1.yaml
+echo "** Input your github username: "
+read repo_user
+echo ""
+echo "** Changing ArgoCD and Pipeline manifests to your forked repo (https://github.com/$repo_user/Openshift-Multi-Cluster-management/)"
+
+sed -i "s/PacktPublishing/$repo_user/" ./Build/Pipeline/quarkus-build-pi.yaml
+sed -i "s/PacktPublishing/$repo_user/" ./Build/PipelineRun/quarkus-build-pr.yaml
+sed -i "s/PacktPublishing/$repo_user/" ./Deploy/argo-app.yaml
+sed -i "s/PacktPublishing/$repo_user/" ./Deploy/Pipeline/quarkus-build-and-deploy-pi.yaml
+sed -i "s/PacktPublishing/$repo_user/" ./Deploy/PipelineRun/quarkus-build-and-deploy-pr.yaml
+sed -i "s/PacktPublishing/$repo_user/" ./DevSecOps/argo-app-v1.yaml
+sed -i "s/PacktPublishing/$repo_user/" ./DevSecOps/argo-app-v2.yaml
+sed -i "s/PacktPublishing/$repo_user/" ./DevSecOps/Pipeline/quarkus-devsecops-v1-pi.yaml
+sed -i "s/PacktPublishing/$repo_user/" ./DevSecOps/Pipeline/quarkus-devsecops-v2-pi.yaml
+sed -i "s/PacktPublishing/$repo_user/" ./DevSecOps/PipelineRun/quarkus-devsecops-v1-pr.yaml
+sed -i "s/PacktPublishing/$repo_user/" ./DevSecOps/PipelineRun/quarkus-devsecops-v2-pr.yaml
+sed -i "s/PacktPublishing/$repo_user/" ./Multicluster-Deployment/applicationset.yaml
+sed -i "s/PacktPublishing/$repo_user/" ./Multicluster-Deployment/Pipeline/quarkus-multicluster-pi.yaml
+sed -i "s/PacktPublishing/$repo_user/" ./Multicluster-Deployment/PipelineRun/quarkus-multicluster-pr.yaml
 
 echo "** Input the full path to the image in your image registry (e.g.: quay-registry-quay-openshift-operators.apps.ocpcluster.example.com/demo/quarkus-quickstarts): "
 read registry_path

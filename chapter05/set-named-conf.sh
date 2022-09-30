6282b0dedc776cd3524c56f01c3fd5ddf6963774
Giovanni Fontana <23301105+giofontana@users.noreply.github.com>
2022-09-30 15:38:43 -0400

    fix set-named-conf

--- chapter05/set-named-conf.sh
+++ chapter05/set-named-conf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Adding bastion IP (192.168.1.200) in the listen-on port line 
-$ sed -i 's/53 { 127.0.0.1;/53 { 127.0.0.1; 192.168.1.200;/g' /etc/named.conf 
+sed -i 's/53 { 127.0.0.1;/53 { 127.0.0.1; 192.168.1.200;/g' /etc/named.conf 
 
 # Adding the new zones 
-$ sudo cat << EOF >>  /etc/named.conf 
+sudo cat << EOF >>  /etc/named.conf 
 zone "ocp.hybridmycloud.com" IN { 
 type master; 
 file "/var/named/ocp.hybridmycloud.com.db"; 
